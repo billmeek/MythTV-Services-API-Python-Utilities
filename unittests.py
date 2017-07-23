151ddffe3173b54600d44760788bc2e76b40689f
Bill Meek <keemllib@gmail.com>
2017-07-23 01:52:20 -0500

    Adds setUp(), _form_url() and _validate_postdata()
    Lots of comment improvements plus initial cache testing

--- unittests.py
+++ unittests.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-This may not qualify for true unit tests, but some of the following are
+This may not qualify for true unit tests, but some of the following are.
 
 NOTE: The value of TEST_HOST and TEST_UTC_OFFSET (below) must be changed
 to the backend under test and current time zone offset respectively.
@@ -13,9 +13,31 @@ import unittest
 from mythtv_services_api import (send as api, utilities as util)
 
 global BACKEND
+BACKEND = None
 
 TEST_HOST = 'mc0'
 TEST_UTC_OFFSET = -18000
+TEST_SERVER_VERSION = '29'
+
+REC_STATUS_DATA = [
+    ['Unknown', -17],
+    ['Missed', -11],
+    ['Tuning', -10],
+    ['Recorder Failed', -9],
+    ['Tuner Busy', -8],
+    ['Low Disk Space', -7],
+    ['Manual Cancel', -6],
+    ['Missed', -5],
+    ['Aborted', -4],
+    ['Recorded', -3],
+    ['Recording', -2],
+    ['Currently Recorded', 3],
+    ['Earlier Showing', 4],
+    ['Max Recordings', 5],
+    ['Not Listed', 6],
+    ['Recorder Off-Line', 12],
+    ['Unknown', 13],
+]
 
 def process_command_line():
     '''
@@ -48,6 +70,21 @@ def process_command_line():
 class MythTVServicesAPI(unittest.TestCase):
     ''' Test the MythTV Services API'''
 
+    def setUp(self):
+        '''
+        Called before every one of the following test()s. Guarantee
+        that the session's options are set to their default values
+        and that UTC time is set.
+        '''
+
+        global BACKEND
+
+        if BACKEND:
+            BACKEND.close_session()
+
+        BACKEND = api.Send(host=TEST_HOST)
+        BACKEND.send(endpoint='Dvr/version')
+
     def test_access(self):
         '''
         Test basic backend access
@@ -55,13 +92,14 @@ class MythTVServicesAPI(unittest.TestCase):
         Also, do the get_utc_offset() tests here so that the UTC_OFFSET
         will be set for future tests.
 
-        Alphabetically, this must be the first test.
+        Alphabetically, this function must be 1st (e.g. name any other
+        function that needs test_a...() test_ad...()).
         '''
 
         self.assertEqual(BACKEND.send(endpoint='Dvr/version')['String'], '6.4')
 
-        self.assertTrue(BACKEND.server_version == '29')
-        self.assertFalse(BACKEND.server_version == '0.28')
+        self.assertTrue(BACKEND.server_version == TEST_SERVER_VERSION)
+        self.assertFalse(BACKEND.server_version == '0.26')
 
         self.assertEqual(util.get_utc_offset(backend=BACKEND), TEST_UTC_OFFSET)
         self.assertEqual(util.get_utc_offset(backend=None), -1)
@@ -74,9 +112,6 @@ class MythTVServicesAPI(unittest.TestCase):
         '''
 
         global BACKEND
-
-        # Need to call send() once to set opts if this test is run by itself:
-        self.assertEqual(BACKEND.send(endpoint='Dvr/version')['String'], '6.4')
 
         for key, value in BACKEND.get_opts.items():
             if key is 'timeout':
@@ -116,12 +151,6 @@ class MythTVServicesAPI(unittest.TestCase):
         Test headers with all options False
         '''
 
-        global BACKEND
-
-        BACKEND.close_session()
-        BACKEND = api.Send(host=TEST_HOST)
-        BACKEND.send(endpoint='Myth/version')
-
         headers_with_no_options_set = {
             'Accept-Encoding': 'gzip,deflate',
             'Connection': 'keep-alive',
@@ -133,9 +162,13 @@ class MythTVServicesAPI(unittest.TestCase):
             self.assertEqual(BACKEND.get_headers(header=header), value)
 
     def test_runtime_exceptions(self):
-        ''' Testing Runtime* exceptions '''
+        '''
+        Testing Runtime* exceptions
 
-        # invalid endpoint  combinations
+        In these tests, we're indirectly testing the _form_url() function.
+        '''
+
+        # invalid endpoint combinations
         self.assertRaises(RuntimeError, BACKEND.send)
         self.assertRaises(RuntimeError, BACKEND.send, endpoint='')
         self.assertRaises(RuntimeError, BACKEND.send, endpoint=None)
@@ -143,26 +176,51 @@ class MythTVServicesAPI(unittest.TestCase):
         # bad endpoint, backend will return a 404
         self.assertRaises(RuntimeError, BACKEND.send, 'Myth/InvalidEndpoint')
 
+        # rest and postdata
         args = {'endpoint': 'Myth/version'}
         kwargs = {'rest': 'Who=Cares',
                   'opts': {'wrmi': True},
                   'postdata': {'Some': 'Junk'}}
-        # rest and postdata
         self.assertRaises(RuntimeError, BACKEND.send, *args, **kwargs)
 
         args = {'endpoint': 'Myth/PutSetting'}
 
-        # postdata not a dict and wrmi=False
+        # Now do _validate_postdata()
+
         kwargs = {'postdata': {'Key': 'FakeSetting', 'HostName': TEST_HOST}}
+
+        # postdata not a dict, *kwargs is intentionally missing a *
         self.assertRaises(RuntimeError, BACKEND.send, *args, *kwargs)
+
+        # wrmi=False
         self.assertRaises(RuntimeWarning, BACKEND.send, *args, **kwargs)
 
-        # ..., put more exceptions here...
-        kwargs = {'postdata': {'Key': 'DBSchemaVer', 'HostName': TEST_HOST}}
-        self.assertRaises(RuntimeWarning, BACKEND.send, *args, **kwargs)
+        # Finally, make sure wsdl can't be used with rest or postdata
+
+        kwargs = {'opts': {'wrmi': True, 'wsdl': True},
+                  'rest': 'Who=Cares'}
+        self.assertRaises(RuntimeError, BACKEND.send, *args, **kwargs)
+
+        kwargs = {'opts': {'wrmi': True, 'wsdl': True},
+                  'postdata': {'Some': 'More Junk'}}
+        self.assertRaises(RuntimeError, BACKEND.send, *args, **kwargs)
+
+    def test_form_url(self):
+        '''
+        Test _form_url(), which has no exceptions, just return a URL,
+        which is developed in the setUp() method.
+        '''
+
+        url = 'http://{}:6544/Dvr/version'.format(TEST_HOST)
+        # pylint: disable=protected-access
+        self.assertEqual(BACKEND._form_url(), url)
+        # pylint: enable=protected-access
 
     def test_validate_header(self):
-        ''' Test _validate_header() '''
+        '''
+        Test _validate_header() RuntimeError exceptions
+        '''
+
         # pylint: disable=protected-access
         self.assertRaises(RuntimeError, BACKEND._validate_header, None)
         self.assertRaises(RuntimeError, BACKEND._validate_header, '')
@@ -171,13 +229,15 @@ class MythTVServicesAPI(unittest.TestCase):
         # pylint: enable=protected-access
 
     def test_create_find_time(self):
-        ''' Test create_find_time()
+        '''
+        Test create_find_time()
 
         Name this function alphabetically after get_utc_offset() or else
         the offset won't be set.
         '''
 
         create_find_time_data = {
+            # time: response
             None: None,
             '': None,
             '20170101 00:01:02': -1,
@@ -191,18 +251,22 @@ class MythTVServicesAPI(unittest.TestCase):
             self.assertEqual(util.create_find_time(time), response)
 
     def test_url_encode(self):
-        ''' Test url_encode() '''
+        '''
+        Test url_encode()
+        '''
 
-        encode_tests = {
+        encode_test_data = {
+            #source: response
             None: None,
             '': '',
             'ABCDEFGHIJKLMNOPQRSTUVWXYZ': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
             'abcdefghijklmnopqrstuvwxyz': 'abcdefghijklmnopqrstuvwxyz',
             'Edición': 'Edici%C3%B3n',
-            'A != B': 'A%20%21%3D%20B'
+            'A != B': 'A%20%21%3D%20B',
+            '@#$%^&*()-={}[]': '%40%23%24%25%5E%26%2A%28%29-%3D%7B%7D%5B%5D',
         }
 
-        for source, response in encode_tests.items():
+        for source, response in encode_test_data.items():
             self.assertEqual(util.url_encode(value=source), response)
 
     def test_utc_to_local(self):
@@ -213,10 +277,12 @@ class MythTVServicesAPI(unittest.TestCase):
         timestamps. Finally, test the omityear keyword.
         '''
 
-        self.assertEqual(util.utc_to_local(None), None)
-        self.assertEqual(util.utc_to_local(''), None)
+        self.assertEqual(util.get_utc_offset(backend=BACKEND), TEST_UTC_OFFSET)
+        self.assertIsNone(util.utc_to_local(None), msg='Null utctime')
+        self.assertIsNone(util.utc_to_local(''), msg='Empty utctime')
 
         utc_to_local_data = {
+            # time, response
             None: None,
             '': None,
             '20170101 00:01:02': None,
@@ -231,38 +297,37 @@ class MythTVServicesAPI(unittest.TestCase):
                                            omityear=True), '12-31 19:01:02')
 
     def test_rec_status_to_string(self):
-        ''' Test rec_status_to_string() '''
-
-        rec_status_data = [
-            ['Unknown', -17],
-            ['Missed', -11],
-            ['Tuning', -10],
-            ['Recorder Failed', -9],
-            ['Tuner Busy', -8],
-            ['Low Disk Space', -7],
-            ['Manual Cancel', -6],
-            ['Missed', -5],
-            ['Aborted', -4],
-            ['Recorded', -3],
-            ['Recording', -2],
-            ['Currently Recorded', 3],
-            ['Earlier Showing', 4],
-            ['Max Recordings', 5],
-            ['Not Listed', 6],
-            ['Recorder Off-Line', 12],
-            ['Unknown', 13],
-        ]
+        '''
+        Test rec_status_to_string()
+        '''
 
         self.assertEqual(util.rec_status_to_string(None), None)
         self.assertEqual(util.rec_status_to_string(backend=None), None)
 
-        for expect, rec_status in rec_status_data:
+        for expect, rec_status in REC_STATUS_DATA:
+            self.assertEqual(util.rec_status_to_string(backend=BACKEND,
+                                                       rec_status=rec_status),
+                             expect)
+
+    def test_rec_status_to_string_cache(self):
+        '''
+        Test rec_status_to_string() for cached strings
+
+        Same as previous test, but the strings should be in cache now. The
+        only way to check is to look at the backend log (with -v http turned
+        on.) Watch for: Connection -1 closed. 17 requests were handled from
+        the above.
+        '''
+
+        for expect, rec_status in REC_STATUS_DATA:
             self.assertEqual(util.rec_status_to_string(backend=BACKEND,
                                                        rec_status=rec_status),
                              expect)
 
     def test_rec_type_to_string(self):
-        ''' Test rec_type_to_string() '''
+        '''
+        Test rec_type_to_string()
+        '''
 
         self.assertEqual(util.rec_type_to_string(backend=None,
                                                  rec_type=1), None)
@@ -271,23 +336,27 @@ class MythTVServicesAPI(unittest.TestCase):
         #self.assertEqual(util.rec_type_to_string(backend=BACKEND,
         #                                         rec_type=None), None)
 
-        rec_type_data = [
-            ['Single Record', 1],
-            ['Not Recording', 3],
-            ['Record All', 4],
-            ['Record Weekly', 5]
-        ]
+        rec_type_data = {
+            # rec_type: expected
+            1: 'Single Record',
+            3: 'Not Recording',
+            4: 'Record All',
+            5: 'Record Weekly',
+        }
 
-        for expected, rec_type in rec_type_data:
+        for rec_type, expected in rec_type_data.items():
             self.assertEqual(util.rec_type_to_string(backend=BACKEND,
                                                      rec_type=rec_type),
                              expected)
 
     def test_dup_method_to_string(self):
-        ''' Test dup_method_to_string() '''
+        '''
+        Test dup_method_to_string()
+        '''
 
-        # TODO: mythbackend isn't handling this endpoint...
+        # TODO: mythbackend isn't handling this endpoint correctly.
         dup_method_and_response = {
+            # method: response
             -5: 'Subtitle and Description',
             -4: 'Subtitle and Description',
             -3: 'Subtitle and Description',
@@ -307,8 +376,6 @@ class MythTVServicesAPI(unittest.TestCase):
 
 if __name__ == '__main__':
 
-    global BACKEND
-    BACKEND = api.Send(host=TEST_HOST)
     unittest.main()
 
 # :!./% --verbose
